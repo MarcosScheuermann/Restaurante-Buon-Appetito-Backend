0e05538883b2798a97af414483ffd60254ff1960
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-18 16:44:09 -0300

    codigolimpio y funcionando

--- controllers/products.js
+++ controllers/products.js
@@ -4,14 +4,8 @@ const User = require("../models/User");
 
 exports.getProducts = async (req, res)=>{
     try {
-      //  const id = req.userId;
-      //  const user = await User.findById(id);
-      //  if(user.role === "ADMIN"){
          const products = await Product.find();
          res.status(200).json({ ok: true, products: products });
-    // }else{
-        // res.status(404).json({ok:false, mensaje:"No tiene permisos"})
-    // }
     } catch (error) {
         console.log(error);
         res.status(400).json({ok:false, mensaje:"Ocurrio un Error"})
