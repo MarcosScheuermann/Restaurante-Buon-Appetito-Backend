49041b938a17ebc0b437fc013079f1e2ac378c6e
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-18 12:06:29 -0300

    rutasprivadas

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 const {Router} = require ("express");
 const { getProduct, addProduct, deleteProduct, getProducts } = require("../controllers/products");
+const { verifyToken } = require("../middlewares/auth");
 const router = Router();
 
-router.get("/", getProducts );
-router.get("/product/:id", getProduct );
-router.post("/",  addProduct);
-router.delete("/:id", deleteProduct);
+router.get("/", verifyToken, getProducts )
+router.get("/product/:id", getProduct )
+router.post("/", addProduct)
+router.delete("/:id", deleteProduct)
 
 
 module.exports = router;
